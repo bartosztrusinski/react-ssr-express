6d3700ef70f743beef586ff48051d3efb7db7743
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-07-23 22:30:30 +0200

    link css in component

--- src/Html.jsx
+++ src/Html.jsx
@@ -7,6 +7,7 @@ export default function Html() {
         <meta charSet='UTF-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <title>Rockets 🚀</title>
+        <link rel='stylesheet' href='bundle.css' />
       </head>
       <body>
         <App />
--- src/renderString.jsx
+++ src/renderString.jsx
@@ -3,13 +3,12 @@ import { renderToString } from 'react-dom/server';
 import Html from './Html';
 
 export default function renderString() {
-  const app = renderToString(<Html />).replace(
-    '</head>',
-    `<link rel="stylesheet" href="bundle.css" />
-    <script type="module" src="bundle.js"></script>
-    </head>`
-  );
-  const html = `<!DOCTYPE html>${app}`;
+  const renderedHtml =
+    '<!DOCTYPE html>' +
+    renderToString(<Html />).replace(
+      '</head>',
+      '<script type="module" src="bundle.js"></script></head>'
+    );
 
-  return html;
+  return renderedHtml;
 }
