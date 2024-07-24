99952cd6ede01f1474a669f81cfdb20fc30a2ea6
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-07-25 00:04:05 +0200

    render correct components

--- src/client.jsx
+++ src/client.jsx
@@ -1,6 +1,12 @@
 import { hydrateRoot } from 'react-dom/client';
 
 import Html from './components/Html';
+import App from './components/App';
 import './style.css';
 
-hydrateRoot(document, <Html />);
+hydrateRoot(
+  document,
+  <Html>
+    <App />
+  </Html>
+);
